3a900ad38bbd0d874f2df2ac207a900efefb59a6
Eric Sampaio <ericsmp18@gmail.com>
2024-02-01 20:14:58 -0300

    ajuste no refresh da pagina

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,54 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        <meta charSet="utf-8" />
-        <link
-          rel="shortcut icon"
-          type="imagex/png"
-          href="../assets/png/faviacon.png"
-        ></link>
-      </Head>
-      <Main />
-      <NextScript />
-    </Html>
-  );
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<any> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            rel="shortcut icon"
+            type="imagex/png"
+            href="../assets/png/faviacon.png"
+          ></link>
+        </Head>
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
 }
